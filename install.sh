b303c8ad9c917969564bef704c6e627916f8c0db
Stanze <daniel.bestandmann@proton.me>
2026-02-13 17:00:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-sudo pacman -S --needed sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg hyprland hyprpicker hyprpaper hyprlock hyprshot bluetui wiremix waybar neovim yazi udiskie udisks2 zoxide swaync rofi noto-fonts noto-fonts-cjk noto-fonts-extra ghostty fzf helvum iwd bazaar btop dhcpcd flatpak firefox ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
+sudo pacman -S --needed sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg hyprland hyprpicker hyprpaper hyprlock hyprshot bluetui wiremix waybar neovim yazi udiskie udisks2 zoxide swaync rofi noto-fonts noto-fonts-cjk noto-fonts-extra ghostty fzf helvum iwd bazaar btop dhcpcd flatpak firefox ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ufw
 
 #copy_files
 
 sudo cp -a ghostty hypr swaync waybar yazi ~/.config/
 
 sudo cp -a rofi ~/.local/share/
+
+
 
