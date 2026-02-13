0ae0abe3ed057536a3c6da4a325876161a54334a
Stanze <daniel.bestandmann@proton.me>
2026-02-13 16:51:54 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S --needed sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg impala hyprland hyprpicker hyprpaper hyprlock hyprshot bluetui wiremix waybar neovim yazi udiskie udisks2 zoxide swaync rofi noto-fonts noto-fonts-cjk noto-fonts-extra ghostty fzf helvum iwd bazaar btop dhcpcd flatpak firefox ttf-jetbrain-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono 
-
-
