02f143627bd23f5c05fdf2fc85989b5d84e7c400
Stanze <daniel.bestandmann@proton.me>
2026-02-13 16:52:57 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pacman -S --needed sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg hyprland hyprpicker hyprpaper hyprlock hyprshot bluetui wiremix waybar neovim yazi udiskie udisks2 zoxide swaync rofi noto-fonts noto-fonts-cjk noto-fonts-extra ghostty fzf helvum iwd bazaar btop dhcpcd flatpak firefox ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
+
+#copy_files
+
+sudo cp -Rf ~/test /home/daniel/Pictures/
