fb51e7ffd17baf3e26363fe91dfd4d5d8dd5cde2
Stanze <daniel.bestandmann@proton.me>
2026-05-11 20:58:38 +0200

    Add files via upload

---
+++ hypr/modules/autostart.lua
@@ -0,0 +1,18 @@
+-------------------
+---- AUTOSTART ----
+-------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+
+hl.on("hyprland.start", function()
+	hl.exec_cmd("udiskie")
+	hl.exec_cmd("awww-daemon")
+	hl.exec_cmd("waybar & swaync")
+	hl.exec_cmd("wl-paste --type text --watch cliphist store")
+	hl.exec_cmd("wl-paste --type image --watch cliphist store")
+	hl.exec_cmd("hypridle")
+	hl.exec_cmd("hyprctl setcursor theme_Breeze_Light 30")
+end)
