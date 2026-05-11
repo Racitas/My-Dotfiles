1206899806508e220c2e9618346a6467e8027300
Stanze <daniel.bestandmann@proton.me>
2026-05-11 20:41:45 +0200

    Add files via upload

---
+++ monitors.lua
@@ -0,0 +1,11 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+	output = "",
+	mode = "2560x1440@144",
+	position = "0x0",
+	scale = "1",
+})
